d11ddf7950cb95f6cda1544181eea4253b667045
Evgeny Simonov <jdsimonov2@gmail.com>
2026-01-08 20:29:33 +0300

    feat: add routes for pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { routes } from './providers/Router';
 
 function App() {
 
---
+++ src/app/providers/Router.ts
@@ -0,0 +1,30 @@
+export const routes = [
+    {
+        name: "Главная",
+        url: '/'
+    },
+    {
+        name: "О сервере",
+        url: '/about'
+    },
+    {
+        name: "Правила",
+        url: '/rules'
+    },
+    {
+        name: "Магазин",
+        url: '/shop'
+    },
+    {
+        name: "Личный кабинет",
+        url: '/profile'
+    },
+    {
+        name: "Авторизация",
+        url: '/auth/login'
+    },
+    {
+        name: "Регистрация",
+        url: '/register'
+    },
+]
