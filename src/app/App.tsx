933ee5a3d97a9810b23b98c3da4f5ebb3a1158cf
Evgeny Simonov <jdsimonov2@gmail.com>
2026-01-10 17:52:07 +0300

    feat: add routing for pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import { routes } from './providers/Router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { routes } from './providers/Router';
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route 
+            key={route.url} 
+            path={route.url} 
+            element={<route.component />} 
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/app/providers/Router.ts
+++ src/app/providers/Router.ts
@@ -1,30 +1,45 @@
+import { AboutPage } from '@pages/AboutPage/AboutPage';
+import { HomePage } from '@pages/HomePage/HomePage';
+import { LoginPage } from '@pages/LoginPage/LoginPage';
+import { ProfilePage } from '@pages/ProfilePage/ProfilePage';
+import { RegisterPage } from '@pages/RegisterPage/RegisterPage';
+import { RulesPage } from '@pages/RulesPage/RulesPage';
+import { ShopPage } from '@pages/ShopPage/ShopPage';
+
 export const routes = [
     {
         name: "Главная",
-        url: '/'
+        url: '/',
+        component: HomePage,
     },
     {
         name: "О сервере",
-        url: '/about'
+        url: '/about',
+        component: AboutPage,
     },
     {
         name: "Правила",
-        url: '/rules'
+        url: '/rules',
+        component: RulesPage,
     },
     {
         name: "Магазин",
-        url: '/shop'
+        url: '/shop',
+        component: ShopPage,
     },
     {
         name: "Личный кабинет",
-        url: '/profile'
+        url: '/profile',
+        component: ProfilePage,
     },
     {
         name: "Авторизация",
-        url: '/auth/login'
+        url: '/auth/login',
+        component: LoginPage,
     },
     {
         name: "Регистрация",
-        url: '/register'
+        url: '/register',
+        component: RegisterPage,
     },
-]
+] as const;
